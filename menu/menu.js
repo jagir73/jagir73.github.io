0cfc81b6838ba43c389dbe419e819f3c0086be01
jagir73 <jasmingiroux@jasmingiroux.com>
2016-08-11 23:04:50 -0400

    Added quick links
    Added quick links

--- menu/menu.js
+++ menu/menu.js
@@ -1,6 +1,8 @@
 document.write('<div id="sideMenu">');
-	document.write('<a href="/"><img width="175px" id="profilepic" src="/menu/menu.jpg" alt="profile picture"/></a>');
-
+	document.write('<a href="/"><img width="175px" id="profilepic" src="/menu/menu.jpg" alt="profile picture"/></a><br />');
+document.write('<a href="http://www.facebook.com/jasmin.giroux"><img src="/images/fb.png" width="16" height="16"" /></a>');
+document.write('<a style="padding-left:10px;" href="https://ca.linkedin.com/in/jasmin-giroux-3799b18b"><img src="/images/in.png" width="16" height="16"" /></a>');
+document.write('<a style="padding-left:10px;" href="mailto:jasmingiroux@jasmingiroux.com"><img src="/images/gmail.png" width="16" height="16"" /></a>');
 document.write('<p>IFT-2103 game projects');
 	document.write('<ul>');
 		document.write('<li><a href="/ChickenTemple">Chicken Temple</a></li>');
