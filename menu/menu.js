3b8ca022fd45652c99989a3b37070067f123c2d4
jagir73 <jasmingiroux@jasmingiroux.com>
2016-08-13 21:40:39 -0400

    Removed most titles + added ninja's tale
    Removed most titles + added ninja's tale

--- menu/menu.js
+++ menu/menu.js
@@ -7,7 +7,7 @@ document.write('<p>IFT-2103 game projects');
 	document.write('<ul>');
 		document.write('<li><a href="/ChickenTemple">Chicken Temple</a></li>');
 		document.write('<li><a href="/WhackamStyle">Whackam Style</a></li>');
-		/*document.write('<li><a href="http://portfolio.joodyp.com/ninja/">Ninja\'s Tale</a></li>');*/
+		document.write('<li><a href="/NinjasTale">Ninja\'s Tale</a></li>');
 	document.write('</ul>');
 document.write('</p>');
 
